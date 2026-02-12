30e1eff8a600efab1a176e7e19b040eb68147d17
Rúben Freitas <joseruben98@hotmail.com>
2026-02-12 09:37:40 +0000

    cast to int

--- app/Http/Resources/TransactionResource.php
+++ app/Http/Resources/TransactionResource.php
@@ -22,13 +22,13 @@ class TransactionResource extends JsonResource
             'type' =>  $this->type,
             'category' => $this->category,
             'user_id' => $this->user_id,
-            'n_clients' => $this->n_clients,
+            'n_clients' => (int) $this->n_clients,
             'guide_name' => $this->guide_name,
             'tracker' => $this->tracker,
             'subCategory' => $this->subCategory,
             'pending' => $this->pending,
-            'willPay' => $this->willPay,
-            'partner' => $this->partner,
+            'willPay' => (int) $this->willPay,
+            'partner' => (int) $this->partner,
         ];
     }
 }
