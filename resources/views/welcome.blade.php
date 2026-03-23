8f6c00c287f52c626e5b3f7bd226652a79eedd5d
Rúben Freitas <joseruben98@hotmail.com>
2026-03-23 15:01:36 +0000

    Refactor Google Tag Manager integration and clean up unused scripts in welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,26 +37,17 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Caveat+Brush&family=Mukta:wght@200;300;400;500;600;700;800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
-    {{-- <script async src="https://www.googletagmanager.com/gtag/js?id=AW-10807342386"></script> --}}
+
+
     <!-- Google Tag Manager -->
     <script>
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-NJP4VFRN');
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-NJP4VFRN');
     </script>
     <!-- End Google Tag Manager -->
-
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-YQ124BK9TB"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-YQ124BK9TB');
-    </script>
 
 
     <title>FastRope</title>
@@ -65,12 +56,12 @@
 <style>
     @font-face {
         font-family: 'Neutrons Demo';
-        src: url('{!! asset('fonts/NeutronsDemoRegular.ttf') !!}');
+        src: url('{!! asset(' fonts/NeutronsDemoRegular.ttf') !!}');
     }
 
     @font-face {
         font-family: 'Palestine Border';
-        src: url('{!! asset('fonts/PalestineBorder.ttf') !!}');
+        src: url('{!! asset(' fonts/PalestineBorder.ttf') !!}');
     }
 
     html,
