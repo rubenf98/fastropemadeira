611a110595498050063e77b8edc0262bd37b5555
Rúben Freitas <joseruben98@hotmail.com>
2026-05-28 18:10:12 +0100

    new gtag

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -49,6 +49,15 @@
   gtag('config', 'G-YQ124BK9TB');
     </script>
 
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-10807342386"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-10807342386');
+    </script>
+
 
     <title>FastRope</title>
 </head>
